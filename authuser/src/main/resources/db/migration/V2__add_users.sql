ffe67bb22e9a04a3eea8b6dadeafd686afd7ae29
Juan <juancarllos.a@gmail.com>
2022-07-08 13:40:08 -0300

    Ajustando app para o heroku - authuser

--- authuser/src/main/resources/db/migration/V2__add_users.sql
+++ authuser/src/main/resources/db/migration/V2__add_users.sql
@@ -1,34 +1,34 @@
 insert into tb_users
 (id, cpf, creation_date, email, full_name, imageurl, last_update_date, "password", phone_number, status, "type", user_name)
-VALUES(gen_random_uuid(), '01383281459', now(), 'admin@gmail.com', 'Admin', null, now(), '123456', '83988046507', 'ACTIVE', 'ADMIN', 'admin.ead');
+VALUES(gen_random_uuid(), '01383281459', now(), 'admin@gmail.com', 'Admin', null, now(), '$2a$10$qIjEL/kurdNyxQkbJ.7TXOdpRkJJEhQdf.qPw1Kk7jkjEfa4GwSu2', '83988046507', 'ACTIVE', 'ADMIN', 'admin.ead');
 
 insert into tb_users
 (id, cpf, creation_date, email, full_name, imageurl, last_update_date, "password", phone_number, status, "type", user_name)
-VALUES(gen_random_uuid(), '01383281458', now(), 'instrutor@gmail.com', 'Instrutor', null, now(), '123456', '83988046508', 'ACTIVE', 'INSTRUCTOR', 'instrutor.ead');
+VALUES(gen_random_uuid(), '01383281458', now(), 'instrutor@gmail.com', 'Instrutor', null, now(), '$2a$10$qIjEL/kurdNyxQkbJ.7TXOdpRkJJEhQdf.qPw1Kk7jkjEfa4GwSu2', '83988046508', 'ACTIVE', 'INSTRUCTOR', 'instrutor.ead');
 
 
 insert into tb_users
 (id, cpf, creation_date, email, full_name, imageurl, last_update_date, "password", phone_number, status, "type", user_name)
-VALUES(gen_random_uuid(), '01383281457', now(), 'juan@gmail.com', 'Juan Carlos', null, now(), '123456', '83988046506', 'ACTIVE', 'STUDENT', 'juan.carlos');
+VALUES(gen_random_uuid(), '01383281457', now(), 'juan@gmail.com', 'Juan Carlos', null, now(), '$2a$10$qIjEL/kurdNyxQkbJ.7TXOdpRkJJEhQdf.qPw1Kk7jkjEfa4GwSu2', '83988046506', 'ACTIVE', 'STUDENT', 'juan.carlos');
 
 insert into tb_users
 (id, cpf, creation_date, email, full_name, imageurl, last_update_date, "password", phone_number, status, "type", user_name)
-VALUES(gen_random_uuid(), '01383281456', now(), 'suelen@gmail.com', 'Suelen Barros', null, now(), '123456', '83988046505', 'ACTIVE', 'STUDENT', 'suelen.barros');
+VALUES(gen_random_uuid(), '01383281456', now(), 'suelen@gmail.com', 'Suelen Barros', null, now(), '$2a$10$qIjEL/kurdNyxQkbJ.7TXOdpRkJJEhQdf.qPw1Kk7jkjEfa4GwSu2', '83988046505', 'ACTIVE', 'STUDENT', 'suelen.barros');
 
 insert into tb_users
 (id, cpf, creation_date, email, full_name, imageurl, last_update_date, "password", phone_number, status, "type", user_name)
-VALUES(gen_random_uuid(), '01383281455', now(), 'suenia@gmail.com', 'Suenia Barros', null, now(), '123456', '83988046504', 'ACTIVE', 'STUDENT', 'suenia.barros');
+VALUES(gen_random_uuid(), '01383281455', now(), 'suenia@gmail.com', 'Suenia Barros', null, now(), '$2a$10$qIjEL/kurdNyxQkbJ.7TXOdpRkJJEhQdf.qPw1Kk7jkjEfa4GwSu2', '83988046504', 'ACTIVE', 'STUDENT', 'suenia.barros');
 
 insert into tb_users
 (id, cpf, creation_date, email, full_name, imageurl, last_update_date, "password", phone_number, status, "type", user_name)
-VALUES(gen_random_uuid(), '01383281454', now(), 'rayra@gmail.com', 'Rayra Mayrla', null, now(), '123456', '83988046503', 'ACTIVE', 'STUDENT', 'rayra.mayrla');
+VALUES(gen_random_uuid(), '01383281454', now(), 'rayra@gmail.com', 'Rayra Mayrla', null, now(), '$2a$10$qIjEL/kurdNyxQkbJ.7TXOdpRkJJEhQdf.qPw1Kk7jkjEfa4GwSu2', '83988046503', 'ACTIVE', 'STUDENT', 'rayra.mayrla');
 
 insert into tb_users
 (id, cpf, creation_date, email, full_name, imageurl, last_update_date, "password", phone_number, status, "type", user_name)
-VALUES(gen_random_uuid(), '01383281453', now(), 'thiago@gmail.com', 'Thiago Araujo', null, now(), '123456', '83988046502', 'ACTIVE', 'STUDENT', 'thiago.araujo');
+VALUES(gen_random_uuid(), '01383281453', now(), 'thiago@gmail.com', 'Thiago Araujo', null, now(), '$2a$10$qIjEL/kurdNyxQkbJ.7TXOdpRkJJEhQdf.qPw1Kk7jkjEfa4GwSu2', '83988046502', 'ACTIVE', 'STUDENT', 'thiago.araujo');
 
 insert into tb_users
 (id, cpf, creation_date, email, full_name, imageurl, last_update_date, "password", phone_number, status, "type", user_name)
-VALUES(gen_random_uuid(), '01383281452', now(), 'maria@gmail.com', 'Maria Silva', null, now(), '123456', '83988046501', 'ACTIVE', 'STUDENT', 'maria.silva');
+VALUES(gen_random_uuid(), '01383281452', now(), 'maria@gmail.com', 'Maria Silva', null, now(), '$2a$10$qIjEL/kurdNyxQkbJ.7TXOdpRkJJEhQdf.qPw1Kk7jkjEfa4GwSu2', '83988046501', 'ACTIVE', 'STUDENT', 'maria.silva');
 
 
